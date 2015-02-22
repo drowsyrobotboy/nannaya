7247221fed67bbf7777d70f919837e5e1e736189
Maruthi Sharma <maruthisharma@live.in>
2015-02-22 17:18:01 +0530

    added Vijay Lakshminarayanan's converter rules

--- scripts/d01a0dd4.main.js
+++
@@ -1,1 +0,0 @@
-$(function(){$("#font-select").selectmenu({select:function(){var a=document.getElementById("font-select").value;$(".content").css({"font-family":a})}}),$("input[type=submit], a, button").button().click(function(a){a.preventDefault()})});
